2ae39d4a174d11874c37b051b81400d1d349c898
SaiSatya16 <jj1039@srmist.edu.in>
2023-10-04 20:10:19 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+# from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+# app = Flask(__name__)
+
+# #configure sqllite3
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+
+# db.init_app(app)
