91a115a04d8ec26474138ae9726722fb51b4e6d9
SaiSatya16 <jj1039@srmist.edu.in>
2023-10-04 22:19:56 +0530

    update

--- app.py
+++ app.py
@@ -1,8 +1,50 @@
-# from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from model import *
 
-# app = Flask(__name__)
 
-# #configure sqllite3
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+#==============================configuration===============================
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Blogs.sqlite3'
+db.init_app(app)
+app.app_context().push()
 
-# db.init_app(app)
+#==============================routes=======================================
+
+@app.route('/')
+def index():
+    #query all blogs
+    b = Blog.query.all()
+    return render_template('index.html', blogs=b)
+
+# /submit-blog
+@app.route('/submit-blog', methods=['POST'])
+def submit_blog():
+    a = request.form['blogTitle']
+    b = request.form['blogContent']
+    blog = Blog(title = a, content=b)
+    db.session.add(blog)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+#delete_blog/{{blog.id}}
+@app.route('/delete_blog/<int:blog_id>')
+def delete_blog(blog_id):
+    blog = Blog.query.get(blog_id)
+    db.session.delete(blog)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -1,3 +1,12 @@
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
-# db = SQLAlchemy()
+#Blog model
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    # def __init__(self, title, content):
+    #     self.title = title
+    #     self.content = content
