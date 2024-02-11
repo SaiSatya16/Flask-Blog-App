a965aa7db2c7a9315deaf55030d49d779d2c40c2
jj1039 <jj1039@srmist.edu.in>
2024-02-11 13:01:00 +0530

    update

--- model.py
+++ model.py
@@ -2,11 +2,26 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 #Blog model
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    blog_relation = db.relationship('Blog', backref='author', secondary="association")
+
+
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
 
-    # def __init__(self, title, content):
-    #     self.title = title
-    #     self.content = content
+
+class Association(db.Model):
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'), primary_key=True, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), primary_key=True, nullable=False)
+
+
+
+
